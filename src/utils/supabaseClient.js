6750af1e97f243306d83331d51ed3cfa4d90a120
Nadhiii <105151919+Nadhiii@users.noreply.github.com>
2026-01-15 17:22:00 +0530

    Update supabaseClient.js

--- src/utils/supabaseClient.js
+++ src/utils/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = https://hputeuurrlygeapzlydn.supabase.co;
-const supabaseAnonKey = sb_publishable_KUT1-X_y8VKIoH6aVpq9fg_qsVBovi9;
+const supabaseUrl = 'https://hputeuurrlygeapzlydn.supabase.co';
+const supabaseAnonKey = 'sb_publishable_KUT1-X_y8VKIoH6aVpq9fg_qsVBovi9';
 
 let supabase = null;
 
